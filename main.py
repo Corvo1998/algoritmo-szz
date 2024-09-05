9ddd362df7ecd0058e94811c8b4999f2eddf3c43
Aline Costa <linecosta04@gmail.com>
2024-09-05 16:23:19 -0300

    2180 code fixed

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_bug_introducing_commits(repo_path, bug_reports):
 repo_path = '/caminho/para/repo'  # Substitua pelo caminho para o seu repositório
 bug_reports = []
 
-print(DAD
+print("testando um novo commit")
 
 bug_introducing_commits = get_bug_introducing_commits(repo_path, bug_reports)
 print("Bug-introducing commits:")
