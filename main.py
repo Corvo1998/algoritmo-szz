02b5464f8fc60a1cfaa4ba909e85fadc067c145d
Douglas Almeida Vidal <vidalstm998@gmail.com>
2024-10-11 14:12:05 -0300

    1069 code error.

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def get_bug_introducing_commits(repo_path, bug_reports):
     return bug_introducing_commits
 
 # Exemplo de uso
-repo_path = '/caminho/para/repo'  # Substitua pelo caminho para o seu repositório
-bug_reports = []
+repo_path = '/caminho/para/rep  # Substitua pelo caminho para o seu repositório
+bug_reports = 
 
-print("TESTE")
+print("TEST
 
 bug_introducing_commits = get_bug_introducing_commits(repo_path, bug_reports)
 print("Bug-introducing commits:")
